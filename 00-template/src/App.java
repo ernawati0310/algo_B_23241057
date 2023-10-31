197963bbbd580d8ed0fb8e6ab0d781538e1b787e
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2023-10-31 13:42:44 +0800

    Kuliah Algo 31 Oktober 2023

---
+++ 00-template/src/App.java
@@ -0,0 +1,37 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       // program Biodata Mahasiswa
+       // deklarasi variabel
+       String nama;
+       String nim;
+       String tgl_lahir;
+       String alamat;
+       float berat_badan;
+       float tinggi_badan;
+
+       //mengisi variabel
+       nama ="erna";
+       nim ="23241057";
+       tgl_lahir ="3 oktober 2003";
+       alamat ="gomong jl.pemuda no.8b";
+       berat_badan =39.5f;
+       tinggi_badan = 150.5f;
+
+       //cetak biodata
+       System.out.println(=================);
+       System.out.println("Nama : " + nama);
+       System.out.println("Nim : " + nim);
+       System.out.println("Tanggal Lahir :" + tgl_lahir);
+       System.out.println("Alamat :" + alamat);
+       System.out.println("Berat Badan :" + berat_badan);
+       System.out.println("Tinggi Badan :" + berat_badan);
+       System.out.println("====================");
+       
+
+
+
+
+
+
+    }
+}
