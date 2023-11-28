06b8c4edb8141f573df41ad692afac0506af7c03
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2023-11-28 13:11:06 +0800

    Tipe Data 28-11-2023

---
+++ 02-tipe data/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+   public static void main(string{} args) trows Exception {
+
+    //tipe data di java
+    //integer, byte, long, double, float, chart, boolean {
+
+    //integer (satuan)
+    int i =10
+    00-template
+    // byte (satuan)
+    byte b =10
+    
+    
+
+
+
+    }
+}
